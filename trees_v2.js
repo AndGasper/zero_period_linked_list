2b72aa270d6442a97d6f9d332a405da6ea5e3303
AndGasper <andy.gasper1@gmail.com>
2017-04-26 16:50:45 -0700

    Attempted to sort 'kittens'

--- trees_v2.js
+++ trees_v2.js
@@ -44,19 +44,35 @@ BinarySearchTree.prototype.push = function(val){
 
 function leftBranch(array) {
     for (let j = parseInt(array.length/2); j >= 0; j--) {
-        bst.push(array[j]);
+        kittensTree.push(array[j]);
     }
 };
 
 function rightBranch(array) {
     for (let i = parseInt(array.length/2); i < array.length; i++) {
-        bst.push(array[i]);
+        kittensTree.push(array[i]);
     }
 };
 
-var bst = new BinarySearchTree();
-var testArray = [0, 14, 28, 42, 53, 69, 84, 92, 128, 137, 156, 170, 171];
-leftBranch(testArray);
-rightBranch(testArray);
-console.log(bst);
+function stringToChar(string) {
+    let stringArray = [];
+    for (let i = 0; i < string.length; i++) {
+        stringArray.push(string.codePointAt(i));
+    }
+    return stringArray.sort();
+}
+
+
+// var bst = new BinarySearchTree();
+// var testArray = [0, 14, 28, 42, 53, 69, 84, 92, 128, 137, 156, 170, 171];
+// leftBranch(testArray);
+// rightBranch(testArray);
+// console.log(bst);
+
+var kittensTree = new BinarySearchTree();
+var test = stringToChar("kittens");
+console.log(test);
+leftBranch(test);
+rightBranch(test);
+console.log(kittensTree);
 
